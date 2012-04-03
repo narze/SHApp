57211bd62b8b7bb3d086b998766978884cb7d9a8
NarzE <manassarn@gmail.com>
2012-04-03 15:39:54 +0700

    Upload randomed pic attached with user's display picture

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,10 +8,17 @@ class Home extends CI_Controller {
 	}
 
 	function index(){
-		
+		if(!$facebook_uid = $this->facebook->getUser()){
+			$this->load->vars('fb_root', $this->fb->getFbRoot());
+			$this->load->view('facebook_connect');
+		} else {			
+			$this->get_random_pic();
+		}
 	}
 
 	function get_random_pic() {
+
+		$facebook_uid = $this->facebook->getUser();
 		$jpgs = glob(FCPATH.'assets/images/random/*.jpg');
 		$pngs = glob(FCPATH.'assets/images/random/*.png');
 		$gifs = glob(FCPATH.'assets/images/random/*.gif');
@@ -19,6 +26,31 @@ class Home extends CI_Controller {
 		$random_image_path = $images[array_rand($images)];
 		$random_image_name = pathinfo($random_image_path, PATHINFO_BASENAME);
 		$random_image_url = base_url().'assets/images/random/'.$random_image_name;
-		echo '<img src="'.$random_image_url.'" />';
+		$user_image = imagecreatefromstring(file_get_contents("http://graph.facebook.com/{$facebook_uid}/picture"));
+		$new_user_image_size = 50;
+		$x = 1;
+		$y = 2;
+		if(strrpos($random_image_url, '.png') !== FALSE) {
+			$background_image = imagecreatefrompng($random_image_url);
+		} else if(strrpos($random_image_url, '.jpg') !== FALSE) {
+			$background_image = imagecreatefromjpeg($random_image_url);
+		} else if(strrpos($random_image_url, '.gif') !== FALSE) {
+			$background_image = imagecreatefromgif($random_image_url);
+		}
+		imagecopymerge($background_image, $user_image, $x, $y, 0, 0, $new_user_image_size, $new_user_image_size, 100);
+
+		$filename = sha1('SaLt'.$facebook_uid.'TlAs');
+
+		$image_path = FCPATH.'uploads/'.$filename.'.png';
+		$image_url = base_url().'uploads/'.$filename.'.png';
+		if(is_writable($image_path)) {
+			unlink($image_path);
+		}
+		if(is_writable(FCPATH.'uploads')) {
+			imagepng($background_image, $image_path);
+			echo '<img src="'.base_url().'uploads/'.$filename.'.png" />';
+		} else {
+			echo 'no write perm';
+		}
 	}
 }
