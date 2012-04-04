52a46fc5792916eba89c71fbb226df72723ca853
Noom <noom@figabyte.com>
2012-04-04 14:53:39 +0700

    Added app's url into message

--- application/config/production/socialhappen.php
+++ application/config/production/socialhappen.php
@@ -33,5 +33,5 @@ $config['randomapp_settings'] = array(
 	'profile_image_y' => 27,
 	'profile_image_type' => 'square',
 	'profile_image_facebook_size' => 50,
-	'default_message' => "ล่า ท้า ผี...คุณจะเจอผีแบบไหน? ",
+	'default_message' => "ล่า ท้า ผี...คุณจะเจอผีแบบไหน? \nhttps://www.facebook.com/SocialHappen/app_266924893395731"
 );
