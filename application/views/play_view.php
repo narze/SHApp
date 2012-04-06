7be3fcc956541cb7008ab33e8b444ab47ee1e5e7
Weerapat P <tong@figabyte.com>
2012-04-06 17:18:56 +0700

    Updated css in play view

--- application/views/play_view.php
+++ application/views/play_view.php
@@ -5,12 +5,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>" />
   <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css');?>" />
+  <style type="text/css">
+  	.user-profile {
+  		width:<?php echo $img_size;?>px;
+  		height:<?php echo $img_size;?>px;
+  		position: absolute;
+  		left:<?php echo $img_x;?>px;
+  		top:<?php echo $img_y;?>px;
+  	}
+  </style>
 	<?php $this->load->view('ga'); ?>
 </head>
 <body style="background-color:black">
 	<center>
 		<?php
-		echo '<p style="height:403px;">'.img($image_url).'</p>';
+		echo '<p style="width:403px;height:403px;position:relative;">'.img($image_url);
+		echo '<img class="user-profile" src="https://graph.facebook.com/'.$facebook_uid.'/picture" /></p>';
 		echo form_open('home/upload', array('class'=>'form-inline'));
 		?>
 			<input type="hidden" name="img_name" value="<?php echo $img_name;?>" />
