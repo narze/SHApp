62d6ac7ad54a089824fca2217810ee58a995eee9
NarzE <manassarn@gmail.com>
2012-04-06 13:14:43 +0700

    Added google analytics

--- application/views/like_view.php
+++ application/views/like_view.php
@@ -1,1 +1,9 @@
-<center><img src="<?php echo base_url('assets/images/like.png');?>" /></center>
+<html>
+<head>
+	<title>ล่า ท้า ผี...คุณจะเจอผีอะไร? by SocialHappen</title>
+	<?php $this->load->view('ga'); ?>
+</head>
+<body>
+	<center><img src="<?php echo base_url('assets/images/like.png');?>" /></center>
+</body>
+</html>
--- application/views/upload_view.php
+++ application/views/upload_view.php
@@ -1,10 +1,11 @@
 <html>
 <head>
-	<title></title>
+	<title>ล่า ท้า ผี...คุณจะเจอผีอะไร? by SocialHappen</title>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css');?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css');?>" />
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css');?>" />
+	<?php $this->load->view('ga'); ?>
 </head>
 <body style="background-color:black">
 	<center>
