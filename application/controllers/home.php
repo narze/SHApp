6bd1d8e5c79141e6ea3efd086197e46cf4f8d11d
NarzE <manassarn@gmail.com>
2012-04-20 12:07:11 +0700

    Catch facebook api exception, and remove temp photo Added static_app_enable config
    Conflicts:
    
    	application/config/socialhappen.php
    	application/controllers/home.php

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -181,95 +181,96 @@ class Home extends CI_Controller {
 
 		imagecopymerge($background_image, $user_image, $profile_image_x, $profile_image_y, 0, 0, $profile_image_size, $profile_image_size, 100);
 
-		//insert name
-		$user = $this->facebook->api('me');
-		if(isset($user['name'])) {	
-			//Try caching font (because windows' apache would lock it!)
-			$original_font_file = FCPATH.'assets/fonts/tahoma.ttf';
-			$cached_font_file = FCPATH.'assets/fonts/tahoma.cached.ttf';
-			
-			if(file_exists($cached_font_file)) {
-				$font_file = $cached_font_file;
-			} else if(is_writable(FCPATH.'assets/')) {
-				if(!file_exists($cached_font_file)) {
-					copy($original_font_file, $cached_font_file);
+		$filename = sha1('SaLt'.$facebook_uid.'TlAs');
+
+		$image_path = FCPATH.'uploads/'.$filename.'.png';
+		$image_url = base_url().'uploads/'.$filename.'.png';
+
+		try {
+			//insert name
+			$user = $this->facebook->api('me');
+			if(isset($user['name'])) {	
+				//Try caching font (because windows' apache would lock it!)
+				$original_font_file = FCPATH.'assets/fonts/tahoma.ttf';
+				$cached_font_file = FCPATH.'assets/fonts/tahoma.cached.ttf';
+				
+				if(file_exists($cached_font_file)) {
+					$font_file = $cached_font_file;
+				} else if(is_writable(FCPATH.'assets/')) {
+					if(!file_exists($cached_font_file)) {
+						copy($original_font_file, $cached_font_file);
+					}
+					$font_file = $cached_font_file;
+				} else {
+					$font_file = $original_font_file;
 				}
-				$font_file = $cached_font_file;
-			} else {
-				$font_file = $original_font_file;
+				//Shadow
+				imagettftext($background_image, 15, 0, $profile_image_x + 2, $profile_image_y + $profile_image_size + 25, $white, $font_file, $user['name']);
+				imagettftext($background_image, 15, 0, $profile_image_x, $profile_image_y + $profile_image_size + 23, $grey, $font_file, $user['name']);
 			}
-			//Shadow
-			//imagettftext($background_image, 13, 0, $profile_image_x + $profile_image_size + 15, $profile_image_y + 7, $grey, $font_file, $user['name']);
-			//imagettftext($background_image, 13, 0, $profile_image_x + $profile_image_size + 17, $profile_image_y + 9, $white, $font_file, $user['name']);
-			imagettftext($background_image, 15, 0, $profile_image_x, $profile_image_y + $profile_image_size + 25, $white, $font_file, $user['name']);
-			imagettftext($background_image, 15, 0, $profile_image_x, $profile_image_y + $profile_image_size + 23, $grey, $font_file, $user['name']);
-		}
-
-		$filename = sha1('SaLt'.$facebook_uid.'TlAs');
-
-		$image_path = FCPATH.'uploads/'.$filename.'.png';
-		$image_url = base_url().'uploads/'.$filename.'.png';
-		if(is_writable($image_path)) {
-			unlink($image_path);
-		}
-		if(is_writable(FCPATH.'uploads')) {
-			imagepng($background_image, $image_path);
-			imagedestroy($background_image);
-		} else {
-			exit('Image cannot be saved');
-		}
-
-		//Image file created, next is uploading
-
-		$filename = sha1('SaLt'.$facebook_uid.'TlAs');
-
-		$image_path = FCPATH.'uploads/'.$filename.'.png';
-		$image_url = base_url().'uploads/'.$filename.'.png';
-		if(is_writable($image_path)) {
-			$randomapp_settings = $this->config->item('randomapp_settings');
-			if($user_message = $this->input->post('message')) {
-				$user_message .= "\n\n\n";
-			}
-			$default_message = $randomapp_settings['default_message'];
-			
-			$page_id = $this->config->item('mockuphappen_facebook_page_id');
-			$facebook_app_id = $this->config->item('facebook_app_id');
-			$app_facebook_url = base_url();
-			$this->facebook->setFileUploadSupport(true);
-			$args = array(
-				'message' => $user_message.$default_message."\n".$app_facebook_url,
-				'image' => '@'.$image_path
-			);
-			$data = $this->facebook->api('me/photos', 'POST', $args);
 
 			if(is_writable($image_path)) {
 				unlink($image_path);
 			}
-
-			$user = $this->facebook->api('me');
-			if(isset($user['link'])) {
-				$facebook_link = $user['link'];
-			}	else {
-				$facebook_link = 'https://facebook.com/'.$facebook_uid;
+			if(is_writable(FCPATH.'uploads')) {
+				imagepng($background_image, $image_path);
+				imagedestroy($background_image);
+			} else {
+				exit('Image cannot be saved');
 			}
-			$this->load->vars(array(
-				'facebook_link' => $facebook_link,
-				'app_title' => $randomapp_settings['app_title'],
-				'app_bgcolor' => $randomapp_settings['app_bgcolor'],
-				'static_server_enable' => $static_server_enable,
-				'static_server_path' => $static_server_path
-			));
-			$this->load->view('upload_view');
 
-			$serialized_app_data = base64_encode(json_encode(array(
-				'app_id'=>$this->config->item('app_id'), 
-				'app_secret_key'=> $this->config->item('app_secret_key'), 
-				'user_facebook_id' => $user['id']
-			)));
-			redirect($this->config->item('static_app_url').'?app_data='.$serialized_app_data);
-		} else {
-			//image not found
-			redirect();
+			//Image file created, next is uploading
+
+			if(is_writable($image_path)) {
+				$randomapp_settings = $this->config->item('randomapp_settings');
+				if($user_message = $this->input->post('message')) {
+					$user_message .= "\n\n\n";
+				}
+				$default_message = $randomapp_settings['default_message'];
+				
+				$page_id = $this->config->item('mockuphappen_facebook_page_id');
+				$facebook_app_id = $this->config->item('facebook_app_id');
+				$app_facebook_url = base_url();
+				$this->facebook->setFileUploadSupport(true);
+				$args = array(
+					'message' => $user_message.$default_message."\n".$app_facebook_url,
+					'image' => '@'.$image_path
+				);
+				$data = $this->facebook->api('me/photos', 'POST', $args);
+
+				unlink($image_path);
+
+				if($this->config->item('static_app_enable')) {
+					$serialized_app_data = base64_encode(json_encode(array(
+						'app_id'=>$this->config->item('app_id'), 
+						'app_secret_key'=> $this->config->item('app_secret_key'), 
+						'user_facebook_id' => $user['id']
+					)));
+					redirect($this->config->item('static_app_url').'?app_data='.$serialized_app_data);
+				} else {
+					if(isset($user['link'])) {
+						$facebook_link = $user['link'];
+					}	else {
+						$facebook_link = 'https://facebook.com/'.$facebook_uid;
+					}
+					$this->load->vars(array(
+						'facebook_link' => $facebook_link,
+						'app_title' => $randomapp_settings['app_title'],
+						'app_bgcolor' => $randomapp_settings['app_bgcolor'],
+						'static_server_enable' => $static_server_enable,
+						'static_server_path' => $static_server_path
+					));
+					$this->load->view('upload_view');
+				}
+			} else {
+				//image not found
+				redirect('?image_error=1');
+			}
+		} catch (FacebookApiException $e) {
+			if(is_writable($image_path)) {
+				unlink($image_path);
+			}
+			redirect('?facebook_error=1');
 		}
 	}
 
