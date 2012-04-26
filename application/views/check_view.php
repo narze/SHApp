0391c25d6b9ec09ce1fe158d5f9efc40636623b5
NarzE <manassarn@gmail.com>
2012-04-26 19:07:05 +0700

    Fixed : User not redirected after facebook login due to facebook's domain

--- application/views/check_view.php
+++ application/views/check_view.php
@@ -7,7 +7,14 @@ xmlns:fb="https://www.facebook.com/2008/fbml">
     <div id='fb-root'></div>
 
     <div id="content">
-      Loading ...
+      <div id="loading">Loading ...</div>
+      <div id="login" style="display:none;">
+        <center>
+          <a id="fblogin" onclick="fblogin();" style="cursor:pointer;"><img src="<?php echo base_url('assets/images/start.jpg');?>" /></a>
+          <div><a target="_blank" href="<?php echo base_url('privacy_policy');?>">Privacy Policy</a></div>
+          <div><a target="_blank" href="<?php echo base_url('terms_of_service');?>">Terms of Service</a></div>
+        </center>
+      </div>
     </div>
 
     <script src="<?php echo base_url('assets/js/jquery-1.7.1.min.js');?>"></script>
