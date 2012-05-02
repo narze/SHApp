b4e7eed7d75c040b4f797a4b773a96e7f7a9c4b6
NarzE <manassarn@gmail.com>
2012-05-02 11:30:08 +0700

    Update config & images

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -39,7 +39,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://app2.socialhappen.com/weddingplace/';
+$config['base_url']	= 'https://app2.socialhappen.com/ghost/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/production/socialhappen.php
+++ application/config/production/socialhappen.php
@@ -1,6 +1,6 @@
 <?php
 //SocialHappen
-$config['app_id'] = 10005;
+$config['app_id'] = 10001;
 $config['app_secret_key'] = '41ee728424904e654348ea66949db013';
 $config['api_url'] = 'https://socialhappen.com/api/';
 
@@ -9,15 +9,15 @@ $config['mongodb_username'] = 'sohap';
 $config['mongodb_password'] = 'figyfigy';
 $config['mongodb_host'] = 'localhost';
 $config['mongodb_port'] = 27017;
-$config['mongodb_database'] = 'weddingplace';
+$config['mongodb_database'] = 'ghost';
 
 //Facebook : Fill these if you want to use facebook library
-$config['facebook_app_id'] = '356226374435146';
-$config['facebook_app_secret'] = '0c2e3143c10694271b75388591ac113a';
+$config['facebook_app_id'] = '125984734199028';
+$config['facebook_app_secret'] = '0a7b12697d16233101b6c455960207f5';
 $config['facebook_app_scope'] = 'user_about_me,publish_stream,user_likes,email';
 $config['facebook_force_like'] = TRUE;
 //Set force like app_id if you want to use other app to force page liking
-$config['facebook_force_like_app_id'] = '356226374435146';
+$config['facebook_force_like_app_id'] = '125984734199028';
 
 //MockupHappen : Use only if you want to use app without socialhappen api calls
 //This will mockup all api request and return values corresponding to api names
@@ -30,18 +30,18 @@ $config['facebook_force_like_app_id'] = '356226374435146';
 //}
 $config['mockuphappen_enable'] = TRUE;
 $config['static_server_enable'] = TRUE;
-$config['static_server_path'] = 'http://static.socialhappen.com/apps/weddingplace/';
+$config['static_server_path'] = 'http://static.socialhappen.com/apps/ghost/';
 
 $config['randomapp_settings'] = array(
-	'app_title' => 'คุณจะได้แต่งงานที่ไหน',
-	'profile_image_size' => 70,
-	'profile_image_x' => 42,
-	'profile_image_y' => 22,
+	'app_title' => 'ล่า ท้า ผี คุณจะเจอผีแบบไหน?',
+	'profile_image_size' => 65,
+	'profile_image_x' => 70,
+	'profile_image_y' => 48,
 	'profile_image_type' => 'square',
 	'profile_image_facebook_size' => 50,
-	'default_message' => "คุณจะได้แต่งงานที่ไหน เข้าไปดูได้ที่",
-	'max_ramdom_number' => 16, //Total files (if $config['static_server_enable'] = TRUE)
-	'app_bgcolor' => '#FFF',
+	'default_message' => "ล่า ท้า ผี คุณจะเจอผีแบบไหน? เข้าไปเจอได้ที่นี่",
+	'max_ramdom_number' => 36, //Total files (if $config['static_server_enable'] = TRUE)
+	'app_bgcolor' => '#000',
 	'maximum_times_played' => 3,
 	'cooldown' => 21600
 );
@@ -50,6 +50,6 @@ $config['static_app_enable'] = TRUE;
 $config['static_app_url'] = 'https://apps.socialhappen.com/static/';
 $config['static_app_message'] = 'อัพโหลดเรียบร้อย คลิกเพื่อดูภาพ';
 
-$config['google_analytics_tracking_id'] = 'UA-18943856-7';
+$config['google_analytics_tracking_id'] = 'UA-18943856-2';
 
 $config['userdata_app_url'] = 'https://app2.socialhappen.com/userdata/';
